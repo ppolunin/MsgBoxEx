a5c8043db9f533acb06400643456b41f75bff2a6
p.polunin <pavel.polunin@ismsglobal.ru>
2022-07-07 13:17:08 +0300

    Готовое решение по MessageBoxIndirect

--- AppRes/AppRes.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Windows.Forms;
-
-public static class AppRes
-{
-    public static MessageBoxEx.IconResource LoadIcon(ushort iconId) => MessageBoxEx.IconResource.Load(typeof(AppRes).Assembly, iconId);
-}
